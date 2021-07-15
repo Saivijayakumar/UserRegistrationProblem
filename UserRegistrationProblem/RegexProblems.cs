8db447b37954aae4f79f5f0c7e614f58acebcb31
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-15 19:23:59 +0530

    [SaiVijayaKumar] Validating Email

--- UserRegistrationProblem/RegexProblems.cs
+++ UserRegistrationProblem/RegexProblems.cs
@@ -51,5 +51,26 @@ namespace UserRegistrationProblem
                 }
             }
         }
+        public static void ValidateingEmail()
+        {
+            //array for list of emails
+            string[] email = { "abc@gmail.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com","abc", "abc@.com.my", "abc123@.com.com", "abc123@.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", ".abc@abc.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+            //regex pattern for email
+            string s = @"^abc([+. \-_]{1}\w+)?@[a-z0-9]+\.[a-z]{2,3}(\.[a-z]{2,3})?$";
+            Regex regex = new Regex(s);
+            foreach (string i in email)
+            {
+                Match res = regex.Match(i);
+                if (res.Success)
+                {
+                    Console.WriteLine($"Valid --> {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"InValid --> {i}");
+                }
+            }
+
+        }
     }
 }

0be40d49c064dfb23e4e0b28d6ec33219e900e1c
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-15 17:20:06 +0530

    [SaiVijayaKumar] Commiting Welcome Message

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------>Welcome To Regex Programs<----------");
+            RegexProblems.ValidateingFirstName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistrationProblem/RegexProblems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    class RegexProblems
+    {
+        public static void ValidateingFirstName()
+        {
+            string[] firstName = { "Sai", "Vijaya", "Kumar", "Ram", "ram", "sai", "Sa", "1sa", " Sai", "kFc" };
+            string s = @"^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(s);
+
+            foreach (string i in firstName)
+            {
+                Match res = regex.Match(i);
+                if (res.Success)
+                {
+                    Console.WriteLine($"Valid --> {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"InValid --> {i}");
+                }
+            }
+        }
+    }
+}

e308ffbfa51c340bb4a5e021a62ed1adcb5089c7
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-16 12:26:58 +0530

    [SaiVijayaKumar] Final Code

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -10,8 +10,40 @@ namespace UserRegistrationProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("------>Welcome To Regex Programs<----------");
-            RegexProblems.ValidateingFirstName();
+            Console.WriteLine();
+            Console.WriteLine("\t\t------>Welcome To Regex Programs<----------\n");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" 1.First Name\n 2.Last Name\n 3.Email\n 4.Phone Number\n 5.Password\n");
+            Console.WriteLine("----------------------");
+            Console.Write("Enter Your Option which you want to see: ");
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Posible validation for First Name");
+                    RegexProblems.ValidateingFirstName();
+                    break;
+                case "2":
+                    Console.WriteLine("Posible validation for Last Name");
+                    RegexProblems.ValidateingLastName();
+                    break;
+                case "3":
+                    Console.WriteLine("Posible validation for Email");
+                    RegexProblems.ValidateingEmail();
+                    break;
+                case "4":
+                    Console.WriteLine("Posible validation for Phone Number");
+                    RegexProblems.ValidateingPhoneNumber();
+                    break;
+                case "5":
+                    Console.WriteLine("Posible validation for Password");
+                    RegexProblems.ValidateingPassword();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid choice");
+                    break;
+            }
             Console.ReadLine();
         }
     }
--- UserRegistrationProblem/RegexProblems.cs
+++ UserRegistrationProblem/RegexProblems.cs
@@ -11,11 +11,100 @@ namespace UserRegistrationProblem
     {
         public static void ValidateingFirstName()
         {
-            string[] firstName = { "Sai", "Vijaya", "Kumar", "Ram", "ram", "sai", "Sa", "1sa", " Sai", "kFc" };
+            Console.WriteLine();
+            //array for list of testcases
+            string[] firstName = { "Sai", "Vijaya", "Kumar", "Ram", "123", "sai", "Sa", "1sa", " Sai", "kFc" };
+            //regex for FirstName
             string s = @"^[A-Z]{1}[a-z]{2,}";
             Regex regex = new Regex(s);
 
             foreach (string i in firstName)
+            {
+                Match res = regex.Match(i);
+                if (res.Success)
+                {
+                    Console.WriteLine($"Valid --> {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"InValid --> {i}");
+                }
+            }
+        }
+        public static void ValidateingLastName()
+        {
+            Console.WriteLine();
+            //array for list of testcases
+            string[] lastName = { "Sai", "Vijaya", "Kumar", "Ram", "123", "sai", "Sa", "1sa", " Sai", "kFc" };
+            //regex for LastName
+            string s = @"^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(s);
+
+            foreach (string i in lastName)
+            {
+                Match res = regex.Match(i);
+                if (res.Success)
+                {
+                    Console.WriteLine($"Valid --> {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"InValid --> {i}");
+                }
+            }
+        }
+        public static void ValidateingEmail()
+        {
+            Console.WriteLine();
+            //array for list of emails
+            string[] email = { "abc@gmail.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc", "abc@.com.my", "abc123@.com.com", "abc123@.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", ".abc@abc.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+            //regex pattern for email
+            string s = @"^abc([+. \-_]{1}\w+)?@[a-z0-9]+\.[a-z]{2,3}(\.[a-z]{2,3})?$";
+            Regex regex = new Regex(s);
+            foreach (string i in email)
+            {
+                Match res = regex.Match(i);
+                if (res.Success)
+                {
+                    Console.WriteLine($"Valid --> {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"InValid --> {i}");
+                }
+            }
+
+        }
+        public static void ValidateingPhoneNumber()
+        {
+            Console.WriteLine();
+            //array for list of Numbers
+            string[] mobileNumber = { "91 9866201443", "21 9876543210", "3 7095821", "07 7095821264", "5 8897023502", "67 1234567890" };
+            //regex pattern for Number
+            string s = @"^[1-9]{2}[ ][0-9]{10}$";
+            Regex regex = new Regex(s);
+            foreach (string i in mobileNumber)
+            {
+                Match res = regex.Match(i);
+                if (res.Success)
+                {
+                    Console.WriteLine($"Valid --> {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"InValid --> {i}");
+                }
+            }
+        }
+        public static void ValidateingPassword()
+        {
+            Console.WriteLine();
+            //array for list of passwords
+            string[] password = { "awsvWf&2", "abcQ*d", "sfls@9", "hhfeY&fd2k", "Bran*ra8f" };
+            //regex pattern for Passwords
+            string s = @"^[a-z]*[A-Z][a-z]*.[a-z]*[0-9][a-z]*$";
+            Regex regex = new Regex(s);
+            foreach (string i in password)
             {
                 Match res = regex.Match(i);
                 if (res.Success)
